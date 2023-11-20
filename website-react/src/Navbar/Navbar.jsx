ae159705c324aff7edf89ad846d19bec3145934c
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-11-19 23:03:49 -0600

    Update Navbar.jsx

--- website-react/src/Navbar/Navbar.jsx
+++ website-react/src/Navbar/Navbar.jsx
@@ -3,15 +3,13 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <div className='nav'>
-            <div className="nav-logo">
-                <ul className="nav-menu">
-                    <li>Home</li>
-                    <li>Explore</li>
-                    <li>About</li>
-                    <li className='nav-contact'>Contact</li>
-                </ul>
-            </div>
-
+            <div className="nav-logo">EV-olution</div>
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>Explore</li>
+                <li>About</li>
+                <li className='nav-contact'>Contact</li>
+            </ul>
         </div>
     )
 }
